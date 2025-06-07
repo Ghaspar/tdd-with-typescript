f69ea9b492149a9e2a49303e4cc3f53f38252daa
Lucas <lucas.ghaspar@gmail.com>
2025-06-07 18:10:49 -0300

    refactor: enhance dateRange validation and add overlap method

--- src/domain/value_objects/dateRange.ts
+++ src/domain/value_objects/dateRange.ts
@@ -4,18 +4,28 @@ export class dateRange{
     private readonly endDate: Date;
 
     constructor(startDate: Date, endDate: Date) {
-        if (endDate < startDate) {
-            throw new Error('Data de término não pode ser anterior à data de início');
-        }
+        this.validateDates(startDate, endDate);
 
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
+    private validateDates(startDate: Date, endDate: Date): void {
+        if (!startDate || !endDate) {
+            throw new Error('Datas de início e término são obrigatórias');
+        }
+        if (startDate == endDate) {
+            throw new Error('Data de início e término não podem ser iguais');
+        }
+        if (endDate < startDate) {
+            throw new Error('Data de término não pode ser anterior à data de início');
+        }
+    }
+
     getStartDate(): Date {
         return this.startDate;
     }
-    getEndDate(): Date {    
+    getEndDate(): Date {
         return this.endDate;
     }
 
@@ -23,5 +33,9 @@ export class dateRange{
         const diffTime = this.endDate.getTime() - this.startDate.getTime();
 
         return Math.ceil(diffTime / (1000 * 3600 * 24));
+    }
+
+    overlaps(other: dateRange): boolean {
+        return this.startDate < other.endDate && other.getStartDate() < this.endDate;
     }
 }
--- src/domain/value_objects/date_range.test.ts
+++ src/domain/value_objects/date_range.test.ts
@@ -27,4 +27,31 @@ describe('DateRange value object', () => {
         expect(totalNights).toBe(5);
     });
 
+    it('Deve verificar se dois intervalos de dados se sobrepoem', () => {
+        // TODO: Teria que criar mais variedades de cenários para garantir esse tipo de teste
+        const validdateRange1 = new dateRange(
+            new Date('2024-12-20'), 
+            new Date('2024-12-25')
+        );
+
+        const validdateRange2 = new dateRange(
+            new Date('2024-12-22'), 
+            new Date('2024-12-27')
+        );
+
+        const overlaps = validdateRange1.overlaps(validdateRange2);
+
+        expect(overlaps).toBe(true);
+
+    });
+
+    it('Deve lançar erro se a data de inicio e termino forem iguais', () => {
+
+        const date = new Date('2024-12-20');
+
+        expect(() => {
+            new dateRange(date, date);
+        }).toThrow('Data de início e término não podem ser iguais');
+    });
+
 });
